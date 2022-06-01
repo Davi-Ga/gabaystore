72eb41b97cc6f378ae28f32fde38e20115ad8db7
Davi-Ga <davigaldinoky@gmail.com>
2022-05-31 21:12:45 -0300

    settings alterado para UpperCase

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gabaystore.apps.GabaystoreConfig',
+    'gabaystore.apps.GabayStoreConfig',
 ]
 
 MIDDLEWARE = [
