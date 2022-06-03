315e6724db2c938375903800eb41e62d19687176
Davi-Ga <davigaldinoky@gmail.com>
2022-06-03 18:05:57 -0300

    allowed host adicionado

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','django_upstream','127.0.0.1','0.0.0.0','django','[::1]','*']
 
 
 # Application definition
