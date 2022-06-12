cf9ae4b78aa6f850598ab7a50cd1919281a43ffe
Davi-Ga <davigaldinoky@gmail.com>
2022-06-12 00:33:22 -0300

    redirect adicionado

--- app/app/settings.py
+++ app/app/settings.py
@@ -132,3 +132,6 @@ STATIC_ROOT = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'home'
