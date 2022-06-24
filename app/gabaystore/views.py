4b330923e95482008c905dbb347c4108c279db8f
Davi-Ga <davigaldinoky@gmail.com>
2022-06-24 08:17:14 -0300

    register method added

--- app/gabaystore/views.py
+++ app/gabaystore/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from forms import UserRegisterForm
 
 def login(request):
     return render(request,'user/login.html')
+
+def registro(request):
+    if request.user.is_authenticated:
+        redirect('homePage')
+    else:
+        form=UserRegisterForm()
+        if request.method=='POST':
+            form=UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()    
+                return redirect('loginPage')
+        context={
+            'form':form
+        }
+        return render(request,'user/register.html',context=context)
