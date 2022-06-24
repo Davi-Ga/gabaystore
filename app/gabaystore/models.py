e96c5f13ceb1a612a75b1fbe1e53f1f1f16d07be
Davi-Ga <davigaldinoky@gmail.com>
2022-06-24 09:15:30 -0300

    cloth model created

--- app/gabaystore/models.py
+++ app/gabaystore/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+SIZE_CHOICES=[
+    ('XS','Extra Small'),
+    ('S','Small'),
+    ('M','Medium'),
+    ('L','Large'),
+    ('XL','Extra Large')
+]
+
+class Cloth(models.Model):
+    name=models.CharField(max_length=150,null=False,unique=True)
+    picture=models.ImageField()
+    size=models.CharField(max_length=1,null=False,choices=SIZE_CHOICES)
+    
+    def __str__(self):
+        return self.name
