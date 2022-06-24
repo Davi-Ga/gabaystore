e10176ed83a2ec62e47b48edbb2192c656d03847
Davi-Ga <davigaldinoky@gmail.com>
2022-06-24 08:25:16 -0300

    login method created

--- app/gabaystore/views.py
+++ app/gabaystore/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
 from forms import UserRegisterForm
 
 def login(request):
-    return render(request,'user/login.html')
+    if request.user.is_authenticated:
+        redirect('homePage')
+    else:
+        if request.method=='POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('homePage')
+            else:
+                return #Error mensage
+        context={}
+        return render(request,'user/login.html',context=context)
 
 def registro(request):
     if request.user.is_authenticated:
