ee914415892b51e712d9c96f5b34c5e8372989b8
Davi-Ga <davigaldinoky@gmail.com>
2022-06-25 02:36:58 -0300

    clothing CRUD created

--- app/gabaystore/views.py
+++ app/gabaystore/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,login,logout
 from gabaystore.forms import UserRegisterForm
+from .models import Cloth
+from .forms import ClothingForm
 
 def home(request):
+    Cloth.objects.all()
     return render(request,'gabaystore/home.html')
 
 def login(request):
@@ -22,7 +25,11 @@ def login(request):
         context={}
         return render(request,'user/login.html',context=context)
 
-def registro(request):
+def logout(request):
+    logout(request)
+    return redirect('homePage')
+
+def register(request):
     if request.user.is_authenticated:
         redirect('homePage')
     else:
@@ -36,3 +43,36 @@ def registro(request):
             'form':form
         }
         return render(request,'user/register.html',context=context)
+
+def clothing_add(request):
+    form=ClothingForm()
+    if request.method=='POST':
+        form=ClothingForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('homePage')
+    context={
+        'form':form
+    }
+    return render(request,'gabaystore/cloth_add.html',context=context)
+
+def clothing_delete(request,pk_cloth):
+    pk_cloth=int(pk_cloth)
+    cloth=Cloth.objects.get(id=pk_cloth)
+    cloth.delete()
+    
+    return redirect('homePage')
+
+def clothing_update(request,pk_cloth):
+    pk_cloth=int(pk_cloth)
+    cloth=Cloth.objects.get(id=pk_cloth)
+    form=ClothingForm(request.POST or None,instance=cloth)
+    if request.method=='POST':
+        form=ClothingForm(request.POST,request.FILES,instance=cloth)
+        if form.is_valid():
+            form.save()
+            return redirect('homePage')
+    context={
+        'form':form
+    }
+    return render(request,'gabaystore/cloth_update.html',context=context)
