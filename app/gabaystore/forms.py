786d8e55492222b7082d1f4fbe432e7cb776aae5
Davi-Ga <davigaldinoky@gmail.com>
2022-06-25 02:33:16 -0300

    ClothingForm created

--- app/gabaystore/forms.py
+++ app/gabaystore/forms.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Cloth
+from django import forms
 
 class UserRegisterForm(UserCreationForm):
     
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+        
+class ClothingForm(forms.ModelForm):
+    class Meta:
+        model=Cloth
+        fields=['name','picture','size']
+        widgets={
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'picture':forms.FileInput(attrs={'class':'form-control'}),
+            'size':forms.Select(attrs={'class':'form-control'}),
+        }
