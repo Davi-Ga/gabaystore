a39aa1e2e3ad21a001f90fdad19735c1a4bc2729
Davi-Ga <davigaldinoky@gmail.com>
2022-06-27 08:15:12 -0300

    types defined

--- app/gabaystore/models.py
+++ app/gabaystore/models.py
@@ -8,10 +8,18 @@ SIZE_CHOICES=[
     ('XL','Extra Large')
 ]
 
+TYPES_CHOICES=[
+    ('T-Shirt'),
+    ('Hoodie'),
+    ('Shirt'),
+]
+
+
 class Cloth(models.Model):
     name=models.CharField(max_length=150,null=False,unique=True)
     picture=models.ImageField()
     size=models.CharField(max_length=1,null=False,choices=SIZE_CHOICES)
+    clothing_type=models.CharField(max_length=10,null=False,choices=TYPES_CHOICES)
     
     def __str__(self):
         return self.name
