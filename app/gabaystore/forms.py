0b29b781fb8c1d28b6ac99c60680560d5bf8bfb9
Davi-Ga <davigaldinoky@gmail.com>
2022-06-27 19:43:43 -0300

    widgets defined with new forms

--- app/gabaystore/forms.py
+++ app/gabaystore/forms.py
@@ -4,10 +4,15 @@ from .models import Cloth
 from django import forms
 
 class UserRegisterForm(UserCreationForm):
-    
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+        widgets={ 
+            'username':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.FileInput(attrs={'class':'form-control'}),
+            'password1':forms.Select(attrs={'class':'form-control'}),
+            'password2':forms.Select(attrs={'class':'form-control'}),
+            }
         
 class ClothingForm(forms.ModelForm):
     class Meta:
