aea4af3b9fa8dee579f532d7ff2c39a89d318701
Davi-Ga <davigaldinoky@gmail.com>
2022-06-29 13:26:43 -0300

    new registeruserform

--- app/gabaystore/forms.py
+++ app/gabaystore/forms.py
@@ -3,16 +3,22 @@ from django.contrib.auth.models import User
 from .models import Cloth
 from django import forms
 
-class UserRegisterForm(UserCreationForm):
+class RegisterUserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control form-control-lg'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control form-control-lg'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control form-control-lg'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control form-control-lg'})
+    
     class Meta:
         model=User
         fields=['username','email','password1','password2']
-        widgets={ 
-            'username':forms.TextInput(attrs={'class':'form-control'}),
-            'email':forms.FileInput(attrs={'class':'form-control'}),
-            'password1':forms.Select(attrs={'class':'form-control'}),
-            'password2':forms.Select(attrs={'class':'form-control'}),
-            }
+        help_texts={
+            'username':None,
+            'password1':None,
+            'password2':None,
+        }
         
 class ClothingForm(forms.ModelForm):
     class Meta:
