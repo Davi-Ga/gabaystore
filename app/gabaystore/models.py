6d6b80cd2505b11507e2949f980bdc1b02e4540f
Davi-Ga <davigaldinoky@gmail.com>
2022-06-29 13:26:55 -0300

    choices ajjusted

--- app/gabaystore/models.py
+++ app/gabaystore/models.py
@@ -9,17 +9,20 @@ SIZE_CHOICES=[
 ]
 
 TYPES_CHOICES=[
-    ('T-Shirt'),
-    ('Hoodie'),
-    ('Shirt'),
+    ('T-Shirt','T-Shirt'),
+    ('Hoodie','Hoodie'),
+    ('Shirt','Shirt'),
 ]
 
 
 class Cloth(models.Model):
     name=models.CharField(max_length=150,null=False,unique=True)
     picture=models.ImageField(null=False)
-    size=models.CharField(max_length=1,null=False,choices=SIZE_CHOICES)
-    clothing_type=models.CharField(max_length=10,null=False,choices=TYPES_CHOICES)
+    size=models.CharField(max_length=2,null=False,choices=SIZE_CHOICES)
+    clothing_type=models.CharField(max_length=20,null=False,choices=TYPES_CHOICES)
+    
+    class Meta:
+        db_table = "Cloth" 
     
     def __str__(self):
         return self.name
