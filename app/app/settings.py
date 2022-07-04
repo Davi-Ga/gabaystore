157a09b92dea4864dcef8261272416baa3b5639e
Davi-Ga <davigaldinoky@gmail.com>
2022-07-04 19:30:23 -0300

    stdimage inclued in apps

--- app/app/settings.py
+++ app/app/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'django_filters',
     'fontawesomefree',
+    'stdimage',
 ]
 
 MIDDLEWARE = [
@@ -136,6 +137,10 @@ STATICFILES_DIRS=[
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
