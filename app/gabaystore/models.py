4b1e9a7555b4b130414a6cf0492d91e6444bc392
Davi-Ga <davigaldinoky@gmail.com>
2022-07-09 18:07:56 -0300

    new validators

--- app/gabaystore/models.py
+++ app/gabaystore/models.py
@@ -3,7 +3,7 @@ from django.db.models import signals
 from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.core.validators import MaxValueValidator
-from .validators import validate_file_name, validate_file_extension_and_size, validate_price, validate_quantity
+from .validators import validate_file_name, validate_file_extension_and_size, validate_price, validate_quantity, validate_size, validate_clothing_type, validate_name
 
 SIZE_CHOICES=[
     ('XS','Extra Small'),
@@ -21,10 +21,10 @@ TYPES_CHOICES=[
 
 
 class Cloth(models.Model):
-    name=models.CharField(max_length=150,null=False,unique=True)
+    name=models.CharField(max_length=150,null=False,unique=True,validators=[validate_name])
     picture=models.ImageField(upload_to='images/',null=False,blank=False,validators=[validate_file_name,validate_file_extension_and_size])
-    size=models.CharField(max_length=2,null=False,choices=SIZE_CHOICES)
-    clothing_type=models.CharField(max_length=20,null=False,choices=TYPES_CHOICES)
+    size=models.CharField(max_length=2,null=False,choices=SIZE_CHOICES,validators=[validate_size])
+    clothing_type=models.CharField(max_length=20,null=False,choices=TYPES_CHOICES,validators=[validate_clothing_type])
     price= models.DecimalField(max_digits=10, decimal_places=2,null=False,validators=[validate_price])
     amount=models.PositiveIntegerField(null=False,validators=[MaxValueValidator(30),validate_quantity])
     slug= models.SlugField(max_length=200, null=True, blank=True, editable=False)
