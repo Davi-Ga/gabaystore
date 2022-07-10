b7e5da11e4cdccd34a3203978d67a763bfcf8e6b
Davi-Ga <davigaldinoky@gmail.com>
2022-07-10 02:54:38 -0300

    duplicated admin removed

--- app/gabaystore/urls.py
+++ app/gabaystore/urls.py
@@ -5,7 +5,6 @@ from django.urls import path
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',views.home,name='homePage'),
     
     path('register/',views.register,name='registerPage'),
