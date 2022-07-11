c2a7f4c0a6bf078bf478e5ad919bd016db275455
Davi-Ga <davigaldinoky@gmail.com>
2022-07-11 18:44:32 -0300

    CartOrder added

--- app/gabaystore/models.py
+++ app/gabaystore/models.py
@@ -4,6 +4,7 @@ from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.core.validators import MaxValueValidator
 from .validators import validate_file_name, validate_file_extension_and_size, validate_price, validate_quantity, validate_size, validate_clothing_type, validate_name
+from django.contrib.auth.models import User
 
 SIZE_CHOICES=[
     ('XS','Extra Small'),
@@ -51,20 +52,33 @@ def cloth_pre_save(sender, instance, signal, **kwargs):
 signals.pre_save.connect(cloth_pre_save, sender=Cloth)
 
 class CartOrder(models.Model):
-    user = models.ForeignKey('gabaystore.User', on_delete=models.CASCADE)
-    quantity=models.PositiveIntegerField(null=False,validators=[MaxValueValidator(30),validate_quantity])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     total_price=models.DecimalField(max_digits=10, decimal_places=2,null=False,validators=[validate_price])
+    paid_status=models.BooleanField(default=False)
+    order_status=models.CharField(max_length=30,null=False,choices=STATUS_CHOICES,default='process')
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     
     class Meta:
         db_table = "CartOrder" 
     
+class CartOrderItems(models.Model):
+    order=models.ForeignKey(CartOrder,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(null=False,validators=[validate_quantity])
+    price=models.DecimalField(max_digits=10, decimal_places=2,null=False,validators=[validate_price])
+    invoice=models.CharField(max_length=150,null=False,unique=True)
+    item=models.ForeignKey(Cloth,on_delete=models.CASCADE)
+    picture=models.ImageField(null=False,blank=False,validators=[validate_file_name,validate_file_extension_and_size])
+    
+    class Meta:
+        db_table = "CartOrderItems" 
+    
     def __str__(self):
         return self.cloth.name
     
     def get_absolute_url(self):
-        return reverse("gabaystore:orderPage", args=self.cloth.id)
+        return reverse("gabaystore:detailClothPage", args=self.cloth.id)
     
     def get_total_price(self):
-        return self.cloth.price * self.quantity
+        return self.quantity*self.price
+    
