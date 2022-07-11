b6d454b45afb566a79dd6f0cf958d1c81cdad998
Davi-Ga <davigaldinoky@gmail.com>
2022-07-11 08:41:36 -0300

    new decorators for admin

--- app/gabaystore/decorators.py
+++ app/gabaystore/decorators.py
@@ -21,3 +21,46 @@ def allowed_users(allowed_roles=[]):
                 return HttpResponse('You are not authorized to view this page')
         return wrapper_func
     return decorator
+
+def admin_only(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'customer':
+            return redirect('homePage')
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
+
+def unauthenticated_user_redirect(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('homePage')
+        else:
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
+
+def allowed_users_redirect(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwargs):
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+            if group in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            else:
+                return redirect('homePage')
+        return wrapper_func
+    return decorator
+
+def admin_only_redirect(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+        if group == 'customer':
+            return redirect('homePage')
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+    return wrapper_func
