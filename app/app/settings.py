a2fec92a94312ce64b333cfc26c94a76a9ec60d5
Davi-Ga <davigaldinoky@gmail.com>
2022-07-12 18:20:36 -0300

    aa

--- app/app/settings.py
+++ app/app/settings.py
@@ -143,6 +143,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
