e2e9a37184bf45353be779a89c7f739bd8587ecf
Davi-Ga <davigaldinoky@gmail.com>
2022-07-12 19:18:01 -0300

    aaaaaaaaaaa

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+project_home = 'app/app'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
