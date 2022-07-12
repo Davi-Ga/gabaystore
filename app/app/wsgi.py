afc1bf04fd64226ca83f0000e4ac5423af33d3c8
Davi-Ga <davigaldinoky@gmail.com>
2022-07-12 19:20:53 -0300

    test

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -11,10 +11,6 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-project_home = 'app/app'
-if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
