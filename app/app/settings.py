70ee01b0711854c2c1f538e23d21b8c3d3b200f6
Davi-Ga <davigaldinoky@gmail.com>
2022-07-17 15:58:42 -0300

    debug testing

--- app/app/settings.py
+++ app/app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Configuração para o ambiente de produção
-DEBUG = False
+DEBUG = str(os.getenv('DEBUG'))
 import django_heroku
 
 
@@ -158,7 +158,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Media files
@@ -168,7 +168,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-DISABLE_COLLECTSTATIC=True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
