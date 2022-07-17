38596937965173b42cb6837841620d8e3bc3b567
Davi-Ga <davigaldinoky@gmail.com>
2022-07-17 16:26:51 -0300

    porra

--- app/app/settings.py
+++ app/app/settings.py
@@ -154,6 +154,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
@@ -168,7 +169,6 @@ django_heroku.settings(locals())
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
