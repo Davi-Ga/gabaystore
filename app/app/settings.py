2ecb4a057c0f7f0348d39d7dc264498af7f9289e
Davi-Ga <davigaldinoky@gmail.com>
2022-07-17 15:54:43 -0300

    collect

--- app/app/settings.py
+++ app/app/settings.py
@@ -168,7 +168,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-DISABLE_COLLECTSTATIC=1
+DISABLE_COLLECTSTATIC=True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
