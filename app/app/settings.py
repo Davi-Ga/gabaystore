bfff12e90a0d203aa218d0757fb41d57fac31f97
Davi-Ga <davigaldinoky@gmail.com>
2022-07-17 13:08:15 -0300

    testing url

--- app/app/settings.py
+++ app/app/settings.py
@@ -163,7 +163,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Media files
 MEDIA_URL = '/static/img/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'/static/img/')
+MEDIA_ROOT = os.path.join(BASE_DIR,'/img/')
 #Crispy forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
