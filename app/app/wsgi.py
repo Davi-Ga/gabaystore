30a9bc6ea71d366e169f25e6c8e96ef6281b1502
Davi-Ga <davigaldinoky@gmail.com>
2022-07-17 16:39:55 -0300

    test

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
 
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
