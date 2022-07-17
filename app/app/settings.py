7c137dd3b3ed2ba490b0ba86c98d0d97fe6a00bc
Davi-Ga <davigaldinoky@gmail.com>
2022-07-17 16:33:38 -0300

    aaaaaaaaaaaaaaaaaa

--- app/app/settings.py
+++ app/app/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gabaystore.apps.GabayStoreConfig',
+    'gabaystore.apps.gabayStoreConfig',
     'crispy_forms',
     'crispy_bootstrap5',
     'django_filters',
@@ -154,7 +154,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
@@ -164,7 +163,7 @@ STATIC_ROOT = '/static/'
 #Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #Crispy forms
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
@@ -173,3 +172,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
--- app/gabaystore/apps.py
+++ app/gabaystore/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GabayStoreConfig(AppConfig):
+class gabayStoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gabaystore'
