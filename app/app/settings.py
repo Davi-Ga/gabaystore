01f9eb33a8c48f2bc2a2d3ee3c9a1df6cc7f0c8a
Davi-Ga <davigaldinoky@gmail.com>
2022-07-17 17:00:45 -0300

    test

--- app/app/settings.py
+++ app/app/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gabaystore.apps.gabayStoreConfig',
+    'app.app.gabaystore.apps.gabayStoreConfig',
     'crispy_forms',
     'crispy_bootstrap5',
     'django_filters',
