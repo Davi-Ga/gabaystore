4048d7a47fdea86855ae527ed2eddb894985e480
Davi-Ga <davigaldinoky@gmail.com>
2022-07-21 21:33:02 -0300

    mimetypes inclued

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
+import mimetypes
+mimetypes.add_type('text/javascript', '.js', True)
+mimetypes.add_type('text/html', '.html', True)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
