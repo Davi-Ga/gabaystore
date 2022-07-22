a1a0d046068e3862e48171c1b0a3f22b9cb45034
Davi-Ga <davigaldinoky@gmail.com>
2022-07-22 03:26:47 -0300

    defender removed

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/defender/', include('defender.urls')), # for django-defender
+    #path('admin/defender/', include('defender.urls')), # for django-defender
     path('',include('gabaystore.urls')),
 ]
 
