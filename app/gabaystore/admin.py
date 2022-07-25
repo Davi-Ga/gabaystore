b0b6870dde20a10fac616fdacc1b9d609a8672fd
Davi-Ga <davigaldinoky@gmail.com>
2022-07-25 16:49:45 -0300

    admin atualizado

--- app/gabaystore/admin.py
+++ app/gabaystore/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Cloth
 from .models import Order
-from .models import OrderItem
+from .models import OrderItems
 
 # Register your models here.
 admin.site.register(Cloth)
 admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.register(OrderItems)
