e6a26c42d13ff73eb52461a0167a6905f9cb33cb
Davi-Ga <davigaldinoky@gmail.com>
2022-07-25 16:46:43 -0300

    admin inclued

--- app/gabaystore/admin.py
+++ app/gabaystore/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Cloth
+from .models import Order
+from .models import OrderItem
 
 # Register your models here.
 admin.site.register(Cloth)
+admin.site.register(Order)
+admin.site.register(OrderItem)
