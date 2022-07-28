1154ec275a0bee81e57d24459619889b8d99f143
Davi-Ga <davigaldinoky@gmail.com>
2022-07-28 18:06:05 -0300

    login required inclued

--- app/gabaystore/decorators.py
+++ app/gabaystore/decorators.py
@@ -35,4 +35,10 @@ def admin_only(view_func):
             return HttpResponse('You are not authorized to view this page')
     return wrapper_func
 
-
+def login_required(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return view_func(request, *args, **kwargs)
+        else:
+            return redirect('loginPage')
+    return wrapper_func
