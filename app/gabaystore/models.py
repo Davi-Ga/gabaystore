24d3cf4c379dfa49a5fa53f747ad56bbe83cc804
Davi-Ga <davigaldinoky@gmail.com>
2022-08-02 16:31:23 -0300

    fixed

--- app/gabaystore/models.py
+++ app/gabaystore/models.py
@@ -48,9 +48,17 @@ def cloth_pre_save(sender, instance, signal, **kwargs):
         instance.slug = slugify(instance.name)
 signals.pre_save.connect(cloth_pre_save, sender=Cloth)
     
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=150,null=False,unique=True,validators=[validate_name])
+    email=models.EmailField(max_length=150,null=False,unique=True)   
+    
+    def __str__(self):
+        return self.name
+ 
     
 class Order(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
     total_price=models.DecimalField(max_digits=10, decimal_places=2,null=False,validators=[validate_price])
     date_order=models.DateTimeField(auto_now_add=True)
     paid_status=models.BooleanField(default=False,null=True,blank=False)
@@ -71,8 +79,8 @@ class Order(models.Model):
         total=sum([item.quantity for item in orderitems])
         return total
     
-class OrderItems(models.Model):
-    item=models.ForeignKey(Cloth,on_delete=models.SET_NULL,null=True)
+class OrderItem(models.Model):
+    cloth=models.ForeignKey(Cloth,on_delete=models.SET_NULL,null=True)
     order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
     quantity=models.PositiveIntegerField(default=0,null=True,blank=False)
     date_added=models.DateTimeField(auto_now_add=True)
