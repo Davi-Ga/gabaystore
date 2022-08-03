b2c1c4e6b3e30f2f36a01297c0a54d705dd11194
Davi-Ga <davigaldinoky@gmail.com>
2022-08-03 16:57:12 -0300

    order incluido

--- app/gabaystore/views.py
+++ app/gabaystore/views.py
@@ -22,8 +22,12 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
+        order = {'get_cart_total':0,'get_cart_items':0}
         
-    context={'items':items}
+    context={
+        'items':items, 
+        'order':order
+    }
     return render(request,'gabaystore/cart.html',context=context)
 
 @login_required(login_url='loginPage')
