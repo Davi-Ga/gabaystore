2d260ee46350ca666e5ec0c67d1719745fdff60f
Davi-Ga <davigaldinoky@gmail.com>
2022-08-04 17:37:03 -0300

    updateItem cart incclued

--- app/gabaystore/views.py
+++ app/gabaystore/views.py
@@ -1,3 +1,5 @@
+from itertools import product
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,logout,login
 from django.contrib.auth.decorators import login_required
@@ -8,6 +10,7 @@ from .forms import ClothingForm
 from .models import Cloth
 from .models import OrderItem,Order
 from django.contrib.auth.models import User
+import json
 
 def home(request):
 
@@ -29,6 +32,30 @@ def cart(request):
         'order':order
     }
     return render(request,'gabaystore/cart.html',context=context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    
+    customer = request.user
+    product = Cloth.objects.get(id=productId)
+    
+    order,created = Order.objects.get_or_create(customer=customer, paid_status=False)
+    
+    orderItem,created = OrderItem.objects.get_or_create(order=order,cloth=product)
+    
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+        
+    orderItem.save()
+    
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added',safe=False)
 
 @login_required(login_url='loginPage')
 @allowed_users(allowed_roles=['customer'])
