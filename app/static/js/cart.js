3c130139685d89673b60a7d9f383e90afadbdbfe
Davi-Ga <davigaldinoky@gmail.com>
2022-08-04 17:37:27 -0300

    UserOrder

--- app/static/js/cart.js
+++ app/static/js/cart.js
@@ -11,8 +11,32 @@ for(var i = 0; i < updateBtns.length; i++){
             console.log('User is not logged in');
            
         }else{
-            console.log('User is logged in');
+            updateUserOrder(productId, action);
           
         }
     });
+}
+
+function updateUserOrder(productId, action){
+    console.log('Usuário autenticado, enviando dados...');
+    
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {  
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+
+    .then((response) => {
+       return response.json()
+    })
+
+    .then((data) => {
+        console.log(data)
+    })
+
 }
