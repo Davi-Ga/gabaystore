791b334777de87436daee3a93ec6f54671ee1e03
Davi-Ga <davigaldinoky@gmail.com>
2022-08-10 17:29:54 -0300

    checkout incluido

--- app/gabaystore/views.py
+++ app/gabaystore/views.py
@@ -31,7 +31,25 @@ def home(request):
     }
     return render(request,'gabaystore/home.html',context)
    
-    
+@login_required(login_url='loginPage')
+@allowed_users(allowed_roles=['customer'])
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user
+        order,created = Order.objects.get_or_create(customer=customer, paid_status=False)
+        items = order.orderitem_set.all()
+        cartItems=order.get_cart_items
+    else:
+        items = []
+        order={'get_cart_total':0,'get_cart_items':0}
+        cartItems = order['get_cart_items']
+        
+    context={
+        'items':items,
+        'order':order,
+        'cartItems':cartItems
+    }
+    return render(request,'gabaystore/checkout.html',context=context)
     
 
 @login_required(login_url='loginPage')
