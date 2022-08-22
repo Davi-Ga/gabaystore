b1af34ce19a7e3487d0ba60a5659f1bc50be23d3
Davi-Ga <davigaldinoky@gmail.com>
2022-08-22 16:32:08 -0300

    heroku config

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,12 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 #Carregando o arquivo de configuração .env
+import django_heroku
 from dotenv import load_dotenv
 import os
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,6 +156,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
