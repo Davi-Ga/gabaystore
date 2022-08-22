d6e2cfde177626bb70159a7d9b636d82f8ac2f7d
Davi-Ga <davigaldinoky@gmail.com>
2022-08-22 20:59:01 -0300

    teste

--- app/app/settings.py
+++ app/app/settings.py
@@ -174,8 +174,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 django_heroku.settings(locals())
