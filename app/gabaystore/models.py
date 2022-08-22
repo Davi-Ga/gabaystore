986f45c70cd17f4553b7fe4f408c4a94d23edcd6
Davi-Ga <davigaldinoky@gmail.com>
2022-08-22 19:18:04 -0300

    ajustes no preço

--- app/gabaystore/models.py
+++ app/gabaystore/models.py
@@ -53,7 +53,7 @@ signals.pre_save.connect(cloth_pre_save, sender=Cloth)
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    total_price=models.DecimalField(max_digits=10, decimal_places=2,validators=[validate_price])
+    total_price=models.DecimalField(max_digits=10, decimal_places=2,null=True)
     date_order=models.DateTimeField(auto_now_add=True)
     paid_status=models.BooleanField(default=False,null=True,blank=False)
     transaction_id=models.CharField(max_length=200,null=True)
