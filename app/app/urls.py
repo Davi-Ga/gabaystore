19dbf702e10f013c228d99cff797e7e104f5e329
Davi-Ga <davigaldinoky@gmail.com>
2022-08-23 08:46:26 -0300

    media url e static url

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('admin/defender/', include('defender.urls')), # for django-defender
     path('',include('gabaystore.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings. STATIC_ROOT)
 
