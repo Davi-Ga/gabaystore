f2b78b8262ee3ed99c4b826ff2c057937cb115e8
Davi-Ga <davigaldinoky@gmail.com>
2022-11-02 22:14:07 -0300

    logger adicionado

--- app/app/settings.py
+++ app/app/settings.py
@@ -141,7 +141,24 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+    
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
